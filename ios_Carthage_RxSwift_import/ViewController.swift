328ded9779a19a53707c7bc5b66321ecc3c003ff
penguin4731 <penguin4731@gmail.com>
2021-08-12 00:19:40 +0900

    Initial Commit

---
+++ ios_Carthage_RxSwift_import/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_Carthage_RxSwift_import
+//
+//  Created by Yo Higashida on 2021/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
